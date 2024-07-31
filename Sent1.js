62264dd506dd177cbdfa95383d32f7a2b82a5a24
BDeines1708 <Bdei1708@gmail.com>
2024-07-31 16:39:12 -0700

    update
    update

---
+++ Sent1.js
@@ -0,0 +1,8 @@
+window.alert ("Hello,world")   // creates a window alert
+document.write("Hello, Multiverse") //writes the phrase into the browser
+
+var Sent1 = "  This is the beginning of the string" //var command creates a string
+var Sent2 = " this is the end of the string"
+document.write (Sent1 + Sent2); //concatenates the string into a phrase
+
+document.write( 7+5) //creates a function
