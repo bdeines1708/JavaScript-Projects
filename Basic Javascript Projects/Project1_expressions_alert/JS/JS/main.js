59b2b6daee25f458a672ea692e7cda04f46c5314
BDeines1708 <Bdei1708@gmail.com>
2024-08-02 17:18:19 -0700

    update
    update

---
+++ Basic Javascript Projects/Project1_expressions_alert/JS/JS/main.js
@@ -0,0 +1,6 @@
+function myFunction()  {           //defining the function and naming it
+    var sentence = "I am learning";  //defining the element and giving it a sentence value
+    sentence += " a lot from this book!";
+    document.getElementById("Concatenate").innerHTML = sentence;  //putting the value into the HTML elementFromPoint
+                                                                  //within the "Concatenate" id       
+}
