68745d0c5494df0e95001877d42e6988e711e92d
BDeines1708 <Bdei1708@gmail.com>
2024-08-13 15:27:44 -0700

    update
    update

---
+++ Basic Javascript Projects/Project9_countdown_slideshow/JS/main.js
@@ -0,0 +1,17 @@
+function countdown() {
+    var seconds = document.getElementById("seconds").value;
+
+    function tick() {
+        seconds = seconds - 1
+        timer.innerHTML = seconds;
+        var time = setTimeout (tick, 1000)
+        if (seconds == -1) {
+            alert ("Time's Up!");
+            clearTimeout(time);
+            timer.innerHTML = "";
+        }
+    }
+    tick();
+}
+
+
