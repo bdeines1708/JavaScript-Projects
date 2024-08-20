1eb5a6d47e8f9ec45c5cb538f203c214b51b8904
BDeines1708 <Bdei1708@gmail.com>
2024-08-20 13:59:18 -0700

    update
    update

--- TicTacToe/js/tictactoe.js
+++ TicTacToe/js/tictactoe.js
@@ -172,6 +172,17 @@ function drawWinLine(coordX1, coordY1, coordX2, coordY2) {
         //This method draws everything we laid out above
         c.stroke();
         //This condition checks if we've reached the endpoints.
+        if (x1 <= x2 && y1 <= y2) {
+            //This condition adds 10 to the previous end x endpoint.
+            if (x < x2) { x += 10; }
+            //This condition adds 10 to the previous end y endpoint.
+            if (y < y2) { y += 10; }
+            //This condition is similar to the one above.
+            //This is necessary for the 6, 4, 2 win conditions.
+            if (x >= x2 && y >= y2) { cancelAnimationFrame(animationLoop); }
+        }
+        //This condition is similar to the one above.
+        //This is necessary for the 6, 4, 2 win condition.
         if (x1 <= x2 && y1 >= y2) {
             if  (x < x2)  { x += 10; }
             if  (y > y2)  { y -= 10; }
